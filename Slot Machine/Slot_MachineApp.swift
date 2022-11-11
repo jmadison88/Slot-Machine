7347c9949e92b6a37a23a11395fe063cd01a8fe6
jmadison <jmad99@sbcglobal.net>
2022-11-11 09:51:29 -0600

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Josh Madison on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
