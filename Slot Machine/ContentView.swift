c1abbce97ccde4ce12ca50452eea96d7b31b06dc
jmadison <jmad99@sbcglobal.net>
2022-12-13 09:21:57 -0600

    Added app animations

--- Slot Machine/ContentView.swift
+++ Slot Machine/ContentView.swift
@@ -10,73 +10,71 @@ import SwiftUI
 struct ContentView: View {
     @State private var amount = 1000
     @State private var numbers = [1,2,0]
-    @State private var showAlert = false
+    @State private var rotation = 180.0
     private var symbols = ["Slots 7","Diamond","Lemon"]
     private var betAmount = 5
     var body: some View {
         NavigationView {
-            ZStack {
+            ZStack { //Title and Background
                 Rectangle()
                     .foregroundColor(Color(red: 200/225, green: 150/225, blue: 50/225))
                     .edgesIgnoringSafeArea(.all)
                 VStack {
                     Text("Welcome to Slots!")
-                        .font(Font.custom("Charter", size: 43))
-                    HStack
+                        .font(Font.custom("Futura", size: 28))
+                        .bold()
+                        .padding(.all, 60)
+                        .animation(Animation.easeOut(duration: 0.1))
+                    HStack //Slots
                     {
-                        
                         Image(symbols[numbers[0]])
                             .resizable()
                             .aspectRatio(1, contentMode: .fit)
                             .background(Color.white.opacity(0.5))
                             .cornerRadius(20)
+                            .animation(Animation.easeOut(duration: 0.5))
                         Image(symbols[numbers[1]])
                             .resizable()
                             .aspectRatio(1, contentMode: .fit)
                             .background(Color.white.opacity(0.5))
                             .cornerRadius(20)
+                            .animation(Animation.easeOut(duration: 0.5))
                         Image(symbols[numbers[2]])
                             .resizable()
                             .aspectRatio(1, contentMode: .fit)
                             .background(Color.white.opacity(0.5))
                             .cornerRadius(20)
-                        
-                        
+                            .animation(Animation.easeOut(duration: 0.5))
                     }
-                    Button(action: {
+                    Button(action: { //Spin Button
                         
                         self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
                         
                         self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
-                        
+                       
                         self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
                         
                         if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2]
                         {
-                            if self.numbers[1] == self.numbers[0] && self.numbers[2] == self.numbers[0]
-                            {
-                                self.amount += self.betAmount * 1000
-                            }
-                            else if self.numbers[1] == self.numbers[2] && self.numbers[0] == self.numbers [2]
-                            {
-                                self.amount += self.betAmount * 100
-                            }
-                            else {
-                                self.amount += self.betAmount * 10
-                            }
+                            self.amount += self.betAmount * 100
+                        }
+                        else if self.numbers[0] == self.numbers[1] || self.numbers[1] == self.numbers[2]
+                        {
+                            self.amount += self.betAmount * 5
                         }
                         else {
                             self.amount -= self.betAmount
                         }
-                        
-                    })  {
-                        Text("Spin")
+                
+                    })
+                    {
+                        Text("SPIN")
                     }
-                    .buttonStyle(CustomButtonStyle())
-                    .padding(.all, 20)
+                    .buttonStyle(CustomButtonStyle()) //Extra UI
+                    .padding(.all, 60)
+                    .animation(Animation.easeOut(duration: 0.1))
                     Text("Current Dollars: $\(amount)")
                         .font(Font.custom("Futura", size: 25))
-                        .padding(.all, 30)
                     Text("Out of money?")
                         .padding()
                         .font(Font.custom("Futura", size: 20))
@@ -85,7 +83,7 @@ struct ContentView: View {
                     }
                     .font(Font.custom("Futura", size: 20))
                     Spacer()
-                    NavigationLink("Winning Slots", destination: WinningSlotsView())
+                    NavigationLink("Winning Slots", destination:  WinningSlotsView())
                         .font(Font.custom("Futura", size: 35))
                     
                 }
@@ -95,82 +93,75 @@ struct ContentView: View {
     struct CustomButtonStyle: ButtonStyle {
         func makeBody(configuration: Configuration) -> some View {
             configuration.label
-                .frame(width: 70)
-                .font(Font.custom("Futura", size: 24))
+                .frame(width: 200)
+                .font(Font.custom("Futura", size: 40))
                 .padding()
-                .background(Color(red: 40/225, green: 150/225, blue: 225/225))
+                .background(Color(red: 225/225, green: 50/225, blue: 50/225))
                 .foregroundColor(.black)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 .bold()
         }
     }
     
-    struct WinningSlotsView: View {
+    struct WinningSlotsView: View { //View for winning slots
         var body: some View {
-            VStack {
-                Text("Winning Slots")
-                    .font(.title)
-                HStack (alignment: .center) {
-                    Image("Diamond")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                    Image("Diamond")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                    Image("Diamond")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color(red: 200/225, green: 150/225, blue: 50/225))
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Winning Slots")
+                        .font(Font.custom("Futura", size: 34))
+                        .bold()
+                    HStack
+                    {
+                        Image("Lemon")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.gray.opacity(0.5))
+                            .cornerRadius(20)
+                        Image("Lemon")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.gray.opacity(0.5))
+                            .cornerRadius(20)
+                        Image("Slots 7")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.gray.opacity(0.5))
+                            .cornerRadius(20)
+                    }
+                    Text("Two of a kind is $25")
+                        .font(Font.custom("Futura", size: 34))
+                    HStack
+                    {
+                        Image("Diamond")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.gray.opacity(0.5))
+                            .cornerRadius(20)
+                        Image("Diamond")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.gray.opacity(0.5))
+                            .cornerRadius(20)
+                        Image("Diamond")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.gray.opacity(0.5))
+                            .cornerRadius(20)
+                    }
+                    Text("Three of a kind is $500")
+                        .font(Font.custom("Futura", size: 34))
+                    
                 }
-                Text("Three diamonds are $5,000")
-                HStack
-                {
-                    Image("Slots 7")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                    Image("Slots 7")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                    Image("Slots 7")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                }
-                Text("Three 7's are $500")
-                HStack
-                {
-                    Image("Lemon")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                    Image("Lemon")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                    Image("Lemon")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.gray.opacity(0.5))
-                        .cornerRadius(20)
-                }
-                Text("Three Lemons are $100")
-
             }
         }
     }
 }
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- WinningSlotsView.swift
+++ WinningSlotsView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct WinningSlotsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
-
 struct WinningSlotsView_Previews: PreviewProvider {
     static var previews: some View {
         WinningSlotsView()
