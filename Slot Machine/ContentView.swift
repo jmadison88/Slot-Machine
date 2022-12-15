499ec7eeb80f89b0bd0b6cdc1066573f139c1cb4
jmadison <jmad99@sbcglobal.net>
2022-12-14 18:20:38 -0600

    Finished App

--- Slot Machine/ContentView.swift
+++ Slot Machine/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var amount = 1000
+    @State private var amount = 100
     @State private var numbers = [1,2,0]
     @State private var rotation = 180.0
     private var symbols = ["Slots 7","Diamond","Lemon"]
@@ -32,7 +32,7 @@ struct ContentView: View {
                             .aspectRatio(1, contentMode: .fit)
                             .background(Color.white.opacity(0.5))
                             .cornerRadius(20)
-                            .animation(Animation.easeOut(duration: 0.5))
+                            .animation(Animation.easeOut(duration: 0.5)) //My own unique addition
                         Image(symbols[numbers[1]])
                             .resizable()
                             .aspectRatio(1, contentMode: .fit)
@@ -46,33 +46,35 @@ struct ContentView: View {
                             .cornerRadius(20)
                             .animation(Animation.easeOut(duration: 0.5))
                     }
-                    Button(action: { //Spin Button
+                    Button(action: { //Spin Button (CodeWithChris Tutorial)
                         
                         self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
                         
                         self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
-                       
+                        
                         self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
                         
                         if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2]
                         {
                             self.amount += self.betAmount * 100
                         }
+                        
                         else if self.numbers[0] == self.numbers[1] || self.numbers[1] == self.numbers[2]
                         {
                             self.amount += self.betAmount * 5
                         }
+                        
                         else {
                             self.amount -= self.betAmount
                         }
-                
+                        
                     })
                     {
                         Text("SPIN")
                     }
                     .buttonStyle(CustomButtonStyle()) //Extra UI
                     .padding(.all, 60)
-                    .animation(Animation.easeOut(duration: 0.1))
+                    .animation(Animation.easeOut(duration: 0.5))
                     Text("Current Dollars: $\(amount)")
                         .font(Font.custom("Futura", size: 25))
                     Text("Out of money?")
@@ -90,15 +92,16 @@ struct ContentView: View {
             }
         }
     }
+    
     struct CustomButtonStyle: ButtonStyle {
         func makeBody(configuration: Configuration) -> some View {
             configuration.label
                 .frame(width: 200)
                 .font(Font.custom("Futura", size: 40))
                 .padding()
-                .background(Color(red: 225/225, green: 50/225, blue: 50/225))
+                .background(.red).opacity(configuration.isPressed ? 0.2 : 1.0)
                 .foregroundColor(.black)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .clipShape(RoundedRectangle(cornerRadius: 40))
                 .bold()
         }
     }
@@ -159,8 +162,6 @@ struct ContentView: View {
         }
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
